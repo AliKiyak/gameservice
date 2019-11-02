a36f0bc3fb710c0ac62e662fb3d63bf42c8e156d
AliKiyak <r0702374@student.thomasmore.be>
2019-11-02 14:52:52 +0100

    added findgamebyid

--- src/main/java/be/thomasmore/team14/game/repository/GameRepository.java
+++ src/main/java/be/thomasmore/team14/game/repository/GameRepository.java
@@ -2,6 +2,11 @@ package be.thomasmore.team14.game.repository;
 
 import be.thomasmore.team14.game.entity.Game;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
 public interface GameRepository extends MongoRepository<Game, String> {
+    List<Game> findGameById(@Param("id") String id);
 
 }
